796d7f0434bdebbac01e12ca0e24d9d0750d2125
jbb3r <57500157+jbb3r@users.noreply.github.com>
2020-09-21 10:48:04 -0400

    notes for the videos

---
+++ app.js
@@ -0,0 +1,36 @@
+const life = 100; // constant stays the same you can't make const=1000
+// var lets you change the value but const stays the same
+
+// instead just use let [name]
+
+
+
+/* this is the old way of creating variables
+
+var life = 100; //notes, this is not computed 
+
+life = life - 10 ;
+var name = 'Dev ed';
+
+var checkout = true;
+
+var box = null;
+console.log(life) ;
+
+variables are boxes where you can add different types of data.
+-how to write a variable
+ 1) var 
+
+ make sure to put the quotes to denote names
+
+ whats a boolean?
+ say we have a shopping website and we have a check out button, but we only want the button available once there are products in the basket. 
+  a boolean is a value of true or false
+  imagine it kind of like a switch.
+
+ What is undefined?
+ variable that doesn't have a value, there is a variable but it's not defined
+
+ WHat is null?
+ variable has non value, it is not commonly used. has a value of nothing?
+*/
