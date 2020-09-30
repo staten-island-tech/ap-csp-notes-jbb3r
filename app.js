8ce9be8e3016629a2bd982334558db3babf36bbf
jbb3r <57500157+jbb3r@users.noreply.github.com>
2020-09-30 10:59:53 -0400

    asdh

--- app.js
+++ app.js
@@ -35,14 +35,93 @@ variables are boxes where you can add different types of data.
  variable has non value, it is not commonly used. has a value of nothing?
 */
 
-const name = "dev ed";
+/*const name = "dev ed";
 
-function logger() {
-    console.log("party time");
-    console.log("party time");
-    console.log("party time");
-    console.log("party time");
-    
+function adder(num1,num2) {
+    console.log(num1+num2);
 }
 
-logger();
+adder(5,10)*/
+
+
+/*string concatenations
+
+const name ="edwin";
+
+console.log('my name is  ${name'); */
+
+
+//number
+/*
+const num1 =100;
+const num2 = 50;
+let val;
+
+//simple math with numbers
+val = num1 + num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 * num2;
+
+//math object? math object
+val =Math.PI;
+val =Math.E;
+Val= Math.round(2.8);
+val= Math.ceil(2.4);
+val= Math.floor(2.8);
+val= Math.sqrt(64);
+val =Math.abs(-3);
+val =Math.pow(8,2);
+val =Math.min(2,3,4,33,2,45);
+val =Math.max(2,3,4,33,2,45);
+val= Math.random();
+
+console.log(val);
+
+
+
+const age = 16;
+
+if (age >= 18) {
+    console.log("you are good to go");
+} else if(age <15){
+    console.log("wow you are really young");
+}
+
+else{
+    console.log("you are not old enough");
+} 
+
+const dice1= 6;
+const dice2= 3;
+
+if (dice1 === 6 && dice2 === 6) {
+    console.log("you are good to go");
+} else {
+    console.log("you are not old enough");
+}
+*/
+
+const use = {
+    name: 'edwin',
+    age :24,
+    married: false,
+    purchases: ["phone", "car", "laptop"]
+
+
+    sayName: function() {
+        console.log(this.name);
+    }
+
+
+};
+
+user.sayName ();
+
+function.sayName();
+
+function saymyAge() {
+    console.log('My age is ${this');
+}
+
+saymyAge();
